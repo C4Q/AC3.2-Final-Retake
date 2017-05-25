2a884db27a2b22dab4cab5fb3263f4938c41617d
Margaret <margaretei@gmail.com>
2017-05-25 18:07:22 -0400

    Final commit!

--- AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/AppDelegate.swift
+++ AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/AppDelegate.swift
@@ -42,16 +42,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        //        if FIRAuth.auth()?.currentUser != nil {
-        //            do {
-        //                try FIRAuth.auth()?.signOut()
-        //                print("logged out")
-        //            } catch {
-        //                print("Error occured while logging out: \(error)")
-        //            }
-        //        }
-        //    }
-        
+        if FIRAuth.auth()?.currentUser != nil {
+            do {
+                try FIRAuth.auth()?.signOut()
+                print("logged out")
+            } catch {
+                print("Error occured while logging out: \(error)")
+            }
+        }
     }
 }
 

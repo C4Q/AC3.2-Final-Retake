63f34fd4c88cae3db2948ccc53d6621daa81f3d7
Margaret <margaretei@gmail.com>
2017-05-25 14:52:41 -0400

    1st test post (a text post) crashes.

--- AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/SecondViewController.swift
+++ AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/SecondViewController.swift
@@ -6,13 +6,16 @@
 //  Copyright © 2017 C4Q. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import FirebaseDatabase
 import FirebaseStorage
+import FirebaseAuth
 
 class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
     var selectedImage: UIImage?
     var selectedText: String?
+    var email: String?
     var databaseReference: FIRDatabaseReference!
 
     @IBOutlet weak var postBarButton: UIBarButtonItem!
@@ -24,12 +27,34 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
         setTextView()
     }
     @IBAction func postBarButtonTapped(_ sender: UIBarButtonItem) {
-    let postImage = selectedImage
-    let postText = postTextView.text
+        let postImage = selectedImage
+        let postText = postTextView.text
+        //https://stackoverflow.com/a/44166954/6526058 referenced stack overflow for why I was getting an error when I did $ else postImage == nil & postText != nil and got an error. I think I understand what the error is, partly, but not how to work around it. So get ready for some chained if statements :-(
         if postImage == nil && postText == "" {
-                self.showOKAlert(title: "Nothing to Post", message: "Please enter a photo OR text to post")
+            self.showOKAlert(title: "Nothing to Post", message: "Please enter a photo OR text to post")
+        }
+        if postImage == nil && postText != "" {
+            //post as text post
+            let postRef = self.databaseReference.childByAutoId()
+            let post = BlogPost(email: email, text: postText, timestamp: Date.init(), type: "Text", userId: email)
+            let postDict = ["email": post.email,
+                            "text": post.text,
+                            "timestamp": post.timestamp,
+                            "type": post.type,
+                            "userId": post.userId] as [String : Any]
+            
+            postRef.setValue(postDict, withCompletionBlock: { (error, reference) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print(reference)
+                    self.showOKAlert(title: "Success!", message: "Photo Uploaded to Meatly Successfuly", completion: nil)
+                    }
+                })
             }
         }
+        
 
     @IBAction func postImageTapped(_ sender: UITapGestureRecognizer) {
         let imagePickerController = UIImagePickerController()

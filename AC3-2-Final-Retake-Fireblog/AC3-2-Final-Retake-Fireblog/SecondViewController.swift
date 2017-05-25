35b8a7fcf38b65c8b32eb294a64d226aadca7b84
Margaret <margaretei@gmail.com>
2017-05-25 15:21:00 -0400

    Both posts and both error messages working on upload.

--- AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/SecondViewController.swift
+++ AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/SecondViewController.swift
@@ -29,7 +29,7 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBAction func postBarButtonTapped(_ sender: UIBarButtonItem) {
         let postImage = selectedImage
         let postText = postTextView.text
-        //https://stackoverflow.com/a/44166954/6526058 referenced stack overflow for why I was getting an error when I did $ else postImage == nil & postText != nil and got an error. I think I understand what the error is, partly, but not how to work around it. So get ready for some chained if statements :-(
+        //https://stackoverflow.com/a/44166954/6526058 referenced stack overflow for why I was getting an error when I did $ else postImage == nil & postText != nil. I think I understand what the error is, partly, but not how to work around it. So get ready for some chained if statements :-(
         if postImage == nil && postText == "" {
             self.showOKAlert(title: "Nothing to Post", message: "Please enter a photo OR text to post")
         }
@@ -51,14 +51,53 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
                 else {
                     print(reference)
                     self.showOKAlert(title: "Success!", message: "Blog Post Uploaded Successfuly", completion: nil)
+                }
+            })
+        }
+        if postImage != nil && postText == "" {
+            //post as image post
+            if let imageData = UIImageJPEGRepresentation(postImage!, 0.5) {
+                
+                let postRef = self.databaseReference.childByAutoId()
+                let storageReference: FIRStorageReference = FIRStorage.storage().reference(forURL: "gs://ac-32-final-retake.appspot.com/")
+                let spaceRef = storageReference.child("images/\(postRef.key)")
+                let metadata = FIRStorageMetadata()
+                let timeStamp = NSNumber(value: Int(NSDate().timeIntervalSince1970))
+                
+                metadata.cacheControl = "public,max-age=300"
+                metadata.contentType = "image/jpeg"
+                
+                _ = spaceRef.put(imageData, metadata: metadata, completion: { (metadata, error) in
+                    if error != nil {
+                        print("Error putting image to storage")
                     }
                 })
+                let post = BlogPost(email: (FIRAuth.auth()?.currentUser?.uid)!, text: "", timestamp: timeStamp, type: "image/jpeg", userId: (FIRAuth.auth()?.currentUser?.uid)!)
+                
+                let postDict = ["email": post.email,
+                                "text": post.text,
+                                "timestamp": post.timestamp,
+                                "type": post.type,
+                                "userId": post.userId] as [String : Any]
+                
+                postRef.setValue(postDict, withCompletionBlock: { (error, reference) in
+                    if let error = error {
+                        print(error)
+                    }
+                    else {
+                        print(reference)
+                        self.showOKAlert(title: "Success!", message: "Photo Uploaded to Meatly Successfuly", completion: nil)
+                    }
+                })
+                
             }
-        if postImage != nil && postText != nil {
+        }
+        
+        if postImage != nil && postText != "" {
             self.showOKAlert(title: "Too much to Post", message: "Please enter a photo OR text to post, not both")
         }
     }
-        
+    
 
     @IBAction func postImageTapped(_ sender: UITapGestureRecognizer) {
         let imagePickerController = UIImagePickerController()

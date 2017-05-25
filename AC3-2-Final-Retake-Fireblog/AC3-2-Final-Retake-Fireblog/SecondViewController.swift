f4575a7c41efec28d7b0f498e784f6622448748d
Margaret <margaretei@gmail.com>
2017-05-25 13:26:59 -0400

    Image View white, image view and text view connected to 2nd vc

--- AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/SecondViewController.swift
+++ AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/SecondViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var postTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func postImageTapped(_ sender: UITapGestureRecognizer) {
+    print("Image Tapped")
     }
-
 
 }
 

8b994728c8900f46455e52403852618a744016f8
Margaret <margaretei@gmail.com>
2017-05-25 11:44:33 -0400

    Register button works.

--- AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/LogInViewController.swift
+++ AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/LogInViewController.swift
@@ -7,20 +7,52 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func logInButtonTapped(_ sender: UIButton) {
     }
-    
+
+    @IBAction func registerButton(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user: FIRUser?, error: Error?) in
+                if error != nil {
+                    print("error with completion while creating new Authentication: \(error!)")
+                }
+                if user != nil {
+                    self.showOKAlert(title: "Registration Succesful", message: nil) {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                } else {
+                    self.showOKAlert(title: "Registration Failed", message: error?.localizedDescription)
+                }
+                self.registerButton.isEnabled = true
+            })
+        }
+    }
+    func showOKAlert(title: String, message: String?, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "OK", style: .cancel) { (_) in
+            if let completionAction = completion {
+                completionAction()
+            }
+        }
+        alert.addAction(okayAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation

2af8139447ce8a23eb8c512989a676a27731caae
Margaret <margaretei@gmail.com>
2017-05-25 13:53:24 -0400

    image picker works. need to do text field.

--- AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/SecondViewController.swift
+++ AC3-2-Final-Retake-Fireblog/AC3-2-Final-Retake-Fireblog/SecondViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 import FirebaseDatabase
 import FirebaseStorage
 
-class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UITextViewDelegate {
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
     var selectedImage: UIImage?
     var databaseReference: FIRDatabaseReference!
 
+    @IBOutlet weak var postBarButton: UIBarButtonItem!
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var postTextView: UITextView!
     override func viewDidLoad() {
@@ -21,12 +22,25 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
         self.databaseReference = FIRDatabase.database().reference().child("posts")
         setTextView()
     }
+    @IBAction func postBarButtonTapped(_ sender: UIBarButtonItem) {
+    print("Post button tapped")
+    }
 
     @IBAction func postImageTapped(_ sender: UITapGestureRecognizer) {
         let imagePickerController = UIImagePickerController()
         imagePickerController.sourceType = .photoLibrary
         imagePickerController.delegate = self
         self.present(imagePickerController, animated: true, completion: nil)
+    }
+    //MARK: - ImagePickerController Delegate Method
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.postImageView.contentMode = .scaleAspectFit
+            self.postImageView.image = image
+            self.selectedImage = image
+        }
+        self.dismiss(animated: true, completion: nil)
     }
 
     func setTextView() {
